d62b86a28efb018910696744fbad7305d9c7d29f
1argeD <gkswhdgur1234@naver.com>
2022-12-29 18:46:48 +0900

    <feat> kafka start

---
+++ src/main/java/com/example/kafkadrills/KafkaDrillsApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkadrills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDrillsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDrillsApplication.class, args);
+    }
+
+}
